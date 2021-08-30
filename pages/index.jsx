165e69ad0373f89847639fc77e689228ffb3bb2e
Sulaiman Rahman <rsulaiman451@gmail.com>
2021-08-30 15:28:54 +0100

    Add tab titles to home and feed pages

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,17 +1,24 @@
-import { Toolbar } from "../components/Toolbar";
+import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import { Toolbar } from "../components/Toolbar";
 
 export default function Home() {
   return (
-    <div className="page-container">
-      <Toolbar />
+    <>
+      <Head>
+        <title>Next News</title>
+      </Head>
+      <div className="page-container">
+        <Toolbar />
 
-      <div className={styles.main}>
-        <h1>Next News</h1>
-        <h3>
-          A Next.js app for accessing the latest news articles via the News API.
-        </h3>
+        <div className={styles.main}>
+          <h1>Next News</h1>
+          <h3>
+            A Next.js app for accessing the latest news articles via the News
+            API.
+          </h3>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
